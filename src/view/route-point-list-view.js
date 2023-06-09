14b153366d92edda905092bd82333f8c8d9338b4
ITsPOLY <ukolova_p@inbox.ru>
2023-06-09 23:48:43 +0300

    Меняй-удаляй

--- src/view/route-point-list-view.js
+++ src/view/route-point-list-view.js
@@ -1,7 +1,6 @@
 import AbstractView from '../framework/view/abstract-view';
 
 const createTemplateTripPointList = () => ('<ul class="trip-events__list"></ul>');
-
 export default class TripPointList extends AbstractView {
   get template() {
     return createTemplateTripPointList();
