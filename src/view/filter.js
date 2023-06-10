c6eb384f47678350e67d92f555b4d57f92601f5b
ITsPOLY <ukolova_p@inbox.ru>
2023-06-10 06:09:33 +0300

    rework

--- src/view/filter.js
+++ src/view/filter.js
@@ -14,6 +14,7 @@ const createFilterTemplate = () => {
         <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything" checked>
         <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
       </div>
+
       <div class="trip-filters__filter">
         <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future">
         <label class="trip-filters__filter-label" for="filter-future">Future</label>
